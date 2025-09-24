b2e0b505193dc570b70c279c32f83a62bc4982d1
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2025-09-24 14:27:58 +0900

    commit

---
+++ monolith/src/main/java/labshopgateway/domain/Order.java
@@ -0,0 +1,54 @@
+package labshopgateway.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import labshopgateway.MonolithApplication;
+import labshopgateway.domain.OrderPlaced;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String customerId;
+
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist() {
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        labshopgateway.external.DecreaseStockCommand decreaseStockCommand = new labshopgateway.external.DecreaseStockCommand();
+        // mappings goes here
+        MonolithApplication.applicationContext
+            .getBean(labshopgateway.external.InventoryService.class)
+            .decreaseStock(/* get???(), */decreaseStockCommand);
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
---
+++ monolith/src/main/java/labshopgateway/domain/OrderRepository.java
@@ -0,0 +1,10 @@
+package labshopgateway.domain;
+
+import labshopgateway.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//<<< PoEAA / Repository
+@RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
+public interface OrderRepository
+    extends PagingAndSortingRepository<Order, Long> {}
